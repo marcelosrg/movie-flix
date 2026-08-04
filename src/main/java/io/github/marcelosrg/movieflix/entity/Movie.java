dd762c4b54691316a40217ab3402c8e1327e0d13
marcelosrg <marcelohrs2005@gmail.com>
2026-08-04 11:17:06 -0300

    feat: adiciona a entidade movie

--- src/main/java/io/github/marcelosrg/movieflix/entity/Movie.java
+++ src/main/java/io/github/marcelosrg/movieflix/entity/Movie.java
@@ -1,4 +1,38 @@
 package io.github.marcelosrg.movieflix.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="movie")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private  String title;
+    private  String description;
+    private  double rating;
+
+    @Column(name= "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name= "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name= "updated_at")
+    private LocalDateTime updatedAt;
 }
