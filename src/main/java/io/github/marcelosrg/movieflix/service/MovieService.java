e3f190f29410362a465d5fc7e3eb3d856485cc9f
marcelosrg <marcelohrs2005@gmail.com>
2026-08-04 12:53:39 -0300

    fix: corrige o retorno ao criar um movie que antes o nome da category e do streaming estavam vindo como null

--- src/main/java/io/github/marcelosrg/movieflix/service/MovieService.java
+++ src/main/java/io/github/marcelosrg/movieflix/service/MovieService.java
@@ -2,29 +2,66 @@ package io.github.marcelosrg.movieflix.service;
 
 import io.github.marcelosrg.movieflix.dtos.request.MovieRequest;
 import io.github.marcelosrg.movieflix.dtos.response.MovieResponse;
+import io.github.marcelosrg.movieflix.entity.Category;
+import io.github.marcelosrg.movieflix.entity.Movie;
+import io.github.marcelosrg.movieflix.entity.Streaming;
+import io.github.marcelosrg.movieflix.exception.NotFoundException;
 import io.github.marcelosrg.movieflix.mapper.MovieMapper;
+import io.github.marcelosrg.movieflix.mapper.StreamingMapper;
+import io.github.marcelosrg.movieflix.repository.CategoryRepository;
 import io.github.marcelosrg.movieflix.repository.MovieRepository;
+import io.github.marcelosrg.movieflix.repository.StreamingRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MovieService {
 
     private final MovieRepository movieRepository;
     private final MovieMapper movieMapper;
+    private final CategoryRepository categoryRepository;
+    private final StreamingRepository streamingRepository;
+
     public MovieService(MovieRepository movieRepository,
-                        MovieMapper movieMapper) {
+                        MovieMapper movieMapper,
+                        CategoryRepository categoryRepository,
+                        StreamingRepository streamingRepository) {
         this.movieRepository = movieRepository;
         this.movieMapper = movieMapper;
-    }
-
-    public MovieResponse createMovie(MovieRequest movieRequest) {
-        return movieMapper.toResponse(movieRepository.save(movieMapper.toEntity(movieRequest)));
+        this.categoryRepository = categoryRepository;
+        this.streamingRepository = streamingRepository;
     }
 
     public List<MovieResponse> findAll(){
         return movieMapper.toResponseList(movieRepository.findAll());
+    }
+
+    public MovieResponse createMovie(MovieRequest movieRequest) {
+        Movie movie = movieMapper.toEntity(movieRequest);
+
+        movie.setCategories(findCategories(movieRequest.categories()));
+        movie.setStreamings(findStreamings(movieRequest.streamings()));
+
+        return movieMapper.toResponse(movieRepository.save(movie));
+    }
+
+
+
+    private List<Category> findCategories(List<UUID> categoryIds) {
+        return categoryIds.stream()
+                .map(id -> categoryRepository.findById(id)
+                        .orElseThrow(() -> new NotFoundException("Categoria não encontrada: " + id)))
+                .toList();
+    }
+
+    private List<Streaming> findStreamings(List<UUID> streamingIds) {
+        return streamingIds.stream()
+                .map(id -> streamingRepository.findById(id)
+                        .orElseThrow(() -> new NotFoundException("Streaming não encontrado: " + id)))
+                .toList();
     }
 
 }
