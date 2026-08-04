e4e824a96c40d3f081dad0358cce8053fa98cdc7
marcelosrg <marcelohrs2005@gmail.com>
2026-08-04 10:07:19 -0300

    feat: adiciona mapeamento entre entidade e dto da entidade Streaming

--- src/main/java/io/github/marcelosrg/movieflix/mapper/StreamingMapper.java
+++ src/main/java/io/github/marcelosrg/movieflix/mapper/StreamingMapper.java
@@ -1,4 +1,30 @@
 package io.github.marcelosrg.movieflix.mapper;
+import io.github.marcelosrg.movieflix.dtos.request.StreamingRequest;
+import io.github.marcelosrg.movieflix.dtos.response.StreamingResponse;
+import io.github.marcelosrg.movieflix.entity.Streaming;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class StreamingMapper {
+
+    public Streaming toStreaming(StreamingRequest streamingRequest){
+        Streaming streaming = new Streaming();
+        streaming.setName(streamingRequest.name());
+
+        return streaming;
+    }
+
+    public StreamingResponse toResponse(Streaming streaming) {
+        return new StreamingResponse(
+                streaming.getId(),
+                streaming.getName());
+    }
+
+    public List<StreamingResponse> toResponseList(List<Streaming> streaming) {
+        return streaming.stream()
+                .map(this::toResponse)
+                .toList();
+    }
 }
