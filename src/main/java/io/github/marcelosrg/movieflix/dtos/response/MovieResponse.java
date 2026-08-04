c49fb531dd65a5ad55c9a4d7a35fc20ce6880760
marcelosrg <marcelohrs2005@gmail.com>
2026-08-04 13:10:46 -0300

    fix: corrige a formatação da data do release para dd/MM/yyyy

--- src/main/java/io/github/marcelosrg/movieflix/dtos/response/MovieResponse.java
+++ src/main/java/io/github/marcelosrg/movieflix/dtos/response/MovieResponse.java
@@ -1,5 +1,7 @@
 package io.github.marcelosrg.movieflix.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.UUID;
 public record MovieResponse(UUID id,
                             String title,
                             String description,
+
+                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                             LocalDate releaseDate,
                             double rating,
                             List<CategoryResponse> categories,
