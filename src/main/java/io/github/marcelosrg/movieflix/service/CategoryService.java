3aa95902950982f19f6a2a46fe799c28205e38bf
marcelosrg <marcelohrs2005@gmail.com>
2026-08-04 08:15:12 -0300

    feat: adiicona metodo para buscar por id e deletar categoria

--- src/main/java/io/github/marcelosrg/movieflix/service/CategoryService.java
+++ src/main/java/io/github/marcelosrg/movieflix/service/CategoryService.java
@@ -5,6 +5,8 @@ import io.github.marcelosrg.movieflix.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CategoryService {
@@ -21,5 +23,14 @@ public class CategoryService {
 
     public Category createCategory(Category category){
         return categoryRepository.save(category);
+    }
+
+    public Category findById(UUID id){
+        return categoryRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Categoria não encontrada!"));
+    }
+
+    public void deleteCategory(UUID id){
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Categoria não encontrada!"));
+        categoryRepository.deleteById(id);
     }
 }
