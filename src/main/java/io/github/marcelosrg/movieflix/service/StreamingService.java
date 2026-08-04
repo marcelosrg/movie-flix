639b949d4bc0bf39b984e2e20caeb82f9f03d225
marcelosrg <marcelohrs2005@gmail.com>
2026-08-04 10:07:56 -0300

    feat: adiciona camada de serviço com operações CRUD da entidade Streaming

--- src/main/java/io/github/marcelosrg/movieflix/service/StreamingService.java
+++ src/main/java/io/github/marcelosrg/movieflix/service/StreamingService.java
@@ -1,14 +1,42 @@
 package io.github.marcelosrg.movieflix.service;
-
+import io.github.marcelosrg.movieflix.dtos.request.StreamingRequest;
+import io.github.marcelosrg.movieflix.dtos.response.StreamingResponse;
+import io.github.marcelosrg.movieflix.entity.Streaming;
+import io.github.marcelosrg.movieflix.exception.NotFoundException;
+import io.github.marcelosrg.movieflix.mapper.StreamingMapper;
 import io.github.marcelosrg.movieflix.repository.StreamingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class StreamingService {
 
     private final StreamingRepository streamingRepository;
+    private final StreamingMapper streamingMapper;
 
-    public StreamingService(StreamingRepository streamingRepository) {
+    public StreamingService(StreamingRepository streamingRepository, StreamingMapper streamingMapper) {
         this.streamingRepository = streamingRepository;
+        this.streamingMapper = streamingMapper;
+    }
+
+
+    public List<StreamingResponse> findAll(){
+        return streamingMapper.toResponseList( streamingRepository.findAll());
+    }
+
+
+    public StreamingResponse createCategory(StreamingRequest streamingRequest) {
+        return streamingMapper.toResponse(streamingRepository.save(streamingMapper.toStreaming(streamingRequest)));
+    }
+
+    public StreamingResponse findById(UUID id){
+        return streamingMapper.toResponse(streamingRepository.findById(id).orElseThrow(() -> new NotFoundException("Streaming não encontrada!")));
+    }
+
+    public void deleteCategory(UUID id){
+        Streaming streaming = streamingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Streaming não encontrada!"));
+        streamingRepository.deleteById(id);
     }
 }
